8835e84fb04708da83f47513141df5d3deb83745
AlonsoMartinez8 <alonsomartinez0804@gmail.com>
2024-07-12 20:31:02 +0200

    Data changes

--- src/components/Interaction.jsx
+++ src/components/Interaction.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Interaction() {
   return (
-    <nav className='w-full flex items-center justify-start gap-4 border-t-2 border-slate-900 mt-4'>
-        <article className='flex items-center justify-start gap-2 p-2 relative min-w-20'>
-            <button><i className="text-4xl ri-heart-line"></i></button>
-            <span className='absolute top-1 right-1 -z-50 aspect-square bg-red-300 rounded-full w-6 flex items-center justify-center'>13</span>
-        </article>
+    <nav className="w-full flex items-center justify-center gap-4 py-2 mt-2 text-xl">
+      <Like />
+      <Comment />
     </nav>
-  )
+  );
+}
+
+function Like() {
+  return (
+    <article>
+      <button>
+        0 <i className="ri-thumb-up-line"></i>
+      </button>
+    </article>
+  );
+}
+
+function Comment() {
+  return (
+    <article>
+      <button>
+        0 <i className="ri-chat-4-line"></i>
+      </button>
+    </article>
+  );
 }
