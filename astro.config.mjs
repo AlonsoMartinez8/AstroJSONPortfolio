f0125465234ea59cc1affb61d082bbb756027a83
AlonsoMartinez8 <alonsomartinez0804@gmail.com>
2024-07-16 20:56:40 +0200

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
+import sitemap from '@astrojs/sitemap';
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), db(), sitemap(),],
+  integrations: [react(), tailwind(), db(), sitemap()],
   devToolbar: {
     enabled: false
   },
