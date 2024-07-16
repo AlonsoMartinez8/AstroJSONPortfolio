3a34caad4b467dc05bfa41306ba0c2fce100f4a5
AlonsoMartinez8 <alonsomartinez0804@gmail.com>
2024-07-16 20:47:52 +0200

    SEO Upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), db()],
+  integrations: [react(), tailwind(), db(), sitemap(),],
   devToolbar: {
     enabled: false
   },
